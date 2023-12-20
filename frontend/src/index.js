12640f95aa86c42e4dd41ac9a7d9d1d473efd11a
Vladislav Ulyanov <flynnrcore@yandex.ru>
2023-12-20 17:05:00 +0300

    add error cheking service rollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,27 @@
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { io } from 'socket.io-client';
 
 import init from './init.jsx';
+
+const rollbarConfig = {
+  accessToken: '6b42d6c004e843c7b693b647060858d7',
+  environment: 'testenv',
+};
 
 const app = async () => {
   const soket = io();
   const root = ReactDOM.createRoot(document.getElementById('chat'));
   const vdom = await init(soket);
-  root.render(vdom);
+  root.render(
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        {vdom}
+      </ErrorBoundary>
+    </RollbarProvider>,
+  );
 };
 
 app();
